6fd7f625bbcb417ecf4854e3b0cd5d89ab7ff05c
Henrique Shiraishi <henriqueashiraishi@gmail.com>
2018-08-07 16:20:02 -0300

    Configurando o Rspec no Rails.

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,15 @@ module TestApp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # Gera automaticamente os arquivos de testes
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
+
   end
 end
